e310e2430bb6b3aea079362d056e5a3ef3ab5086
arshdeep-2004 <arshde67@sheridancollege.ca>
2025-04-08 23:57:01 -0400

    Update Card.java

--- src/main/java/com/mycompany/project2/Card.java
+++ src/main/java/com/mycompany/project2/Card.java
@@ -6,24 +6,24 @@ package com.mycompany.project2;
 
 public class Card {
     private String suit;
-    private int value;
+    private int rank;
 
-    public Card(String suit, int value) {
+    public Card(String suit, int rank) {
         this.suit = suit;
-        this.value = value;
+        this.rank = rank;
     }
 
     public String getSuit() {
         return suit;
     }
 
-    public int getValue() {
-        return value;
+    public int getRank() {
+        return rank;
     }
 
     @Override
     public String toString() {
-        return value + " of " + suit;
+        return rank + " of " + suit;
     }
 }
 
